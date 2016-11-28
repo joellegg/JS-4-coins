c221815f384b942469a2e5bfc8281b469b87986f
joellegg <joellegg@bellsouth.net>
2016-11-28 17:44:07 -0600

    added math functions to get coin count

--- coins.js
+++ coins.js
@@ -1,16 +1,3 @@
-/*
-  Input: 0.67
-  Output:
-  {
-    quarters: 2,
-    dimes: 1,
-    nickels: 1,
-    pennies: 2
-  }
-
-*/
-
-
 // function for coin counting
 function coinCounter() {
   // Initialize a JavaScript object to hold the coins
@@ -19,14 +6,30 @@ function coinCounter() {
   coinPurse.dimes = 0;
   coinPurse.nickels = 0;
   coinPurse.pennies = 0;
-  // convert coins * 100 to get rid of decimals  
+  // convert coins * 100 to get rid of decimals 
+  coinInput = coinInput * 100; 
   // divide coins by 25 for quarters
+  if (coinInput >= 25) {
+    coinPurse.quarters = Math.floor(coinInput / 25);
+    coinInput -= (coinPurse.quarters * 25);
+  }
   // divide remainder coins by 10 for dimes
+  if (coinInput >= 10) {
+    coinPurse.dimes = Math.floor(coinInput / 10)
+    coinInput -= (coinPurse.dimes * 10)
+  }
   // divide remainder coins by 5 for nickels
-  // divide remainder coins by 1 for pennies
+  if (coinInput >= 5) {
+    coinPurse.nickels = Math.floor(coinInput / 5)
+    coinInput -= (coinPurse.nickels * 5)
+  }
+  // coinInput should now equal number of pennies
+  if (coinInput >= 1) {
+    coinPurse.pennies = coinInput
+  }
   return coinPurse;
 }
 // get coinInput value from user
-var coinInput = document.getElementById("coinInput").value;
+var coinInput = document.getElementById("coinInput");
 console.log(coinInput);
 coinCounter();
