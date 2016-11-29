802050be2cc91dc28fd06c8cb306248b91a3bfc2
joellegg <joellegg@bellsouth.net>
2016-11-29 13:51:37 -0600

    reordered js to so it would work, added event handler

--- coins.js
+++ coins.js
@@ -1,11 +1,18 @@
 // function for coin counting
 function coinCounter() {
+  // set the result of coinPurse to the HTML
+  var unorderedList = document.getElementById("results");
+  unorderedList.innerHTML = "";
   // Initialize a JavaScript object to hold the coins
-  var coinPurse = {};
-  coinPurse.quarters = 0;
-  coinPurse.dimes = 0;
-  coinPurse.nickels = 0;
-  coinPurse.pennies = 0;
+  coinPurse = {};
+    coinPurse.quarters = 0;
+    coinPurse.dimes = 0;
+    coinPurse.nickels = 0;
+    coinPurse.pennies = 0;
+  // get coinInput value from user and run function
+  var coinInput = document.getElementById("coinInput").value;
+  // print to console the value of the coinInput (in function at beginning so it gets the value)
+  // console.log(coinInput);
   // convert coins * 100 to get rid of decimals 
   coinInput = coinInput * 100; 
   // divide coins by 25 for quarters
@@ -27,20 +34,26 @@ function coinCounter() {
   if (coinInput >= 1) {
     coinPurse.pennies = coinInput
   }
+  // set the result of coinPurse to the HTML
+  unorderedList = document.getElementById("results");
+  // loop through the coinPurse to get each key/property (in function)
+  for (var prop in coinPurse) {
+    // create list item
+    var listItem = document.createElement("li");
+    // add result to list item
+    listItem.innerHTML = prop + ': ' + coinPurse[prop];
+    // add list item to unordered list
+    unorderedList.appendChild(listItem);
+  }
   return coinPurse;
 }
-// set the result of coinPurse to the HTML
-var ul = document.getElementById("coinPurse");
-// loop through the coinPurse to get each key/property
-for (var prop in coinPurse) {
-  // create list item
-  var li = document.createElement("li");
-  // add result to list item
-  li.innerHTML = prop + ': ' + coinPurse[prop];
-  // add list item to unordered list
-  ul.appendChild(li);
-}
-// get coinInput value from user and run function
-var coinInput = document.getElementById("coinInput");
-console.log(coinInput);
-coinCounter();
+
+// add event handler to run the function 
+var buttonChange = document.getElementById("buttonChange");
+// execute function on click
+buttonChange.addEventListener("click", coinCounter);
+
+
+
+
+
